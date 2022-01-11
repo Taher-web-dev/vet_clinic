6a977668853e529b94ec305d423832635dd6d200
Taher <ttaherhagui@gmail.com>
2022-01-11 22:50:10 +0100

    Add queries to answer questions.

--- queries.sql
+++ queries.sql
@@ -30,7 +30,7 @@ ROLLBACK;
 BEGIN;
 DELETE FROM animals WHERE date_of_birth > '2022-01-01';
 SAVEPOINT BB2022;
-UPDATE animals SET weight_kg = weight_kg * -1 ;
+UPDATE animals SET weight_kg = weight_kg * -1 ; 
 ROLLBACK TO BB2022;
 UPDATE animals SEt weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
@@ -49,3 +49,41 @@ FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) AS AVG_ESCAPE_NBR 
 FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01'
 GROUP BY species;
+
+SELECT A.name, O.full_name 
+FROM animals A
+JOIN owners O ON A.owner_id = O.id 
+WHERE O.full_name = 'Melody Pond';
+
+SELECT A.name, A.date_of_birth, A.escape_attempts, A.neutered, A.weight_kg, S.name AS type
+FROM animals A 
+JOIN species S ON A.species_id = S.id
+WHERE S.name = 'Pokemon';
+
+SELECT A.name AS animal_name , O.full_name AS owner 
+FROM animals A 
+RIGHT JOIN owners O ON A.owner_id = O.id ;
+
+SELECT COUNT(*) AS ANIMALS_NBR , S.name AS species
+FROM animals A 
+JOIN species S ON A.species_id = S.id 
+GROUP BY species;
+
+SELECT A.name , A.date_of_birth , A.escape_attempts, A.neutered , A.weight_kg 
+FROM animals A 
+JOIN owners O ON A.owner_id = O.id 
+JOIN species S ON A.species_id = S.id
+WHERE S.name = 'Digimon' AND O.full_name = 'Jennifer Orwell';
+
+SELECT A.name , A.date_of_birth , A.escape_attempts, A.neutered , A.weight_kg
+FROM animals A 
+JOIN owners O ON A.owner_id = O.id 
+WHERE A.escape_attempts = 0 AND O.full_name = 'Dean Winchester';
+
+
+SELECT COUNT(*) AS ANIMALS_NBR, O.full_name 
+FROM animals A 
+JOIN owners O ON A.owner_id = O.id
+GROUP BY (O.full_name) 
+ORDER BY ANIMALS_NBR DESC
+LIMIT 1;

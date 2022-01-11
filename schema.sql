3ef7038369ae0fec621e72a2a5eb8f2345700f9b
Taher <ttaherhagui@gmail.com>
2022-01-11 14:48:11 +0100

    add speci column.

--- schema.sql
+++ schema.sql
@@ -3,7 +3,10 @@ id INT GENERATED ALWAYS AS IDENTITY,
 name VARCHAR(50),
 date_of_birth date,
 escape_attempts INT,
-neutered bit,
+neutered boolean,
 weight_kg decimal,
 PRIMARY KEY (id)
 );
+
+ALTER TABLE animals 
+ADD species VARCHAR(100);
